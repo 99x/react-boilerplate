be7eb6b39751faa206018688926348ba1fae4f84
rajikaimal <inbox.rajika@gmail.com>
2016-07-05 22:54:05 +0530

    watch

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,15 @@ const env = process.env.NODE_ENV || 'development';
 const NODEMODULES = path.resolve(__dirname, 'node_modules');
 
 const config = {
-   entry: path.resolve(__dirname, 'src/main.js'),
+   entry: path.resolve(__dirname, './src/main.js'),
 	
    output: {
-      path:'./',
       filename: path.resolve(__dirname, 'src/index.js'),
    },
 	
    devServer: {
       inline: true,
+      publicPath: '/src/',
       port: 8080
    },
 	
@@ -27,7 +27,8 @@ const config = {
             }
          }
       ]
-   }
+   },
+   watch: true
 }
 
 module.exports = config;
