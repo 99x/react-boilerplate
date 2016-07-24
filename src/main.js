7c8f8e3a5a283d1cdf4415f2099adfd1db0a7d72
rajikaimal <inbox.rajika@gmail.com>
2016-07-24 21:45:41 +0530

    move react components into src/components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './components/app.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
